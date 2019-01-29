46724084e63992915e1b1c0bf9f7438a7e9de370
Lisa Scott <lrscot@aol.com>
2019-01-29 04:05:56 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++
@@ -1,8 +0,0 @@
-puts "Hello World!"
-hellow_world.#!/usr/bin/env ruby -wKU
-
-hello_world.#!/usr/bin/env ruby -wKU
-hellow_world.#!/usr/bin/env ruby -wKU
-puts "Hello Word!"
-hello_world.#!/usr/bin/env ruby -wKU
-rubyhello_world.#!/usr/bin/env ruby -wKU
